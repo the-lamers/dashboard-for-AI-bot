36baf0764900e1aa89c6998ee600c8ed6dc043a7
funtik22 <mega.artemm@bk.ru>
2025-03-04 15:09:35 +0300

    devops docker

--- frontend/src/api/metricsAPI.ts
+++ frontend/src/api/metricsAPI.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_BASE_URL = "http://localhost:5000/api";
 
 export const fetchMetrics = async () => {
-  const response = await axios.get(`${API_BASE_URL}/metrics`);
-  return response.data;
+   const response = await axios.get(`${API_BASE_URL}/metrics`);
+   return response.data;
 };
